6d2e2c3c807f7fd9c243b86a5ffa1d453faa726a
Alyx <75553966+JuiceyBeans@users.noreply.github.com>
2024-05-14 08:31:07 +0100

    Oops actually fixed bio reactor now

--- src/main/java/com/juiceybeans/gthrt/common/data/HRTMachines.java
+++ src/main/java/com/juiceybeans/gthrt/common/data/HRTMachines.java
@@ -9,7 +9,6 @@ import com.gregtechceu.gtceu.api.recipe.OverclockingLogic;
 import com.gregtechceu.gtceu.common.data.GTMachines;
 import com.gregtechceu.gtceu.common.data.GTRecipeModifiers;
 import com.gregtechceu.gtceu.common.data.GTRecipeTypes;
-import com.lowdragmc.lowdraglib.side.fluid.FluidHelper;
 
 import static com.gregtechceu.gtceu.api.GTValues.*;
 import static com.gregtechceu.gtceu.common.data.GTMachines.defaultTankSizeFunction;
@@ -19,7 +18,7 @@ public class HRTMachines {
 
     public static final MachineDefinition[] BIO_REACTOR = GTMachines.registerTieredMachines("bio_reactor", (holder, tier) -> new SimpleTieredMachine(holder, tier, defaultTankSizeFunction), (tier, builder) -> builder
             .langValue("%s Small Bio Reactor %s".formatted(VLVH[tier], VLVT[tier]))
-            .editableUI(SimpleTieredMachine.EDITABLE_UI_CREATOR.apply(GTCEu.id("macerator"), GTRecipeTypes.MACERATOR_RECIPES))
+            .editableUI(SimpleTieredMachine.EDITABLE_UI_CREATOR.apply(GTCEu.id("bio_reactor"), HRTRecipeTypes.BIO_REACTOR_RECIPES))
             .rotationState(RotationState.NON_Y_AXIS)
             .recipeType(HRTRecipeTypes.BIO_REACTOR_RECIPES)
             .recipeModifier(GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.NON_PERFECT_OVERCLOCK))
